305f3f1c91fbf595706b07fa18d5b05700788511
NovaStorm93 <novastorm963@gmail.com>
2024-10-07 00:42:30 -0400

    added method stubs for Language, Languageapp, phrase, settings, and word

---
+++ speek/src/main/java/com/narriation/Phrase.java
@@ -0,0 +1,58 @@
+package speek.src.main.java.com.narriation;
+
+import java.util.HashMap;
+
+public class Phrase {
+    String phrase;
+    String definition;
+    Language language;
+    HashMap<Language,String> translations;
+    
+    public Phrase(String phrase, String definition, String unknown){
+        //TODO: Method Stub
+        // What is the third string value for?
+        
+    }
+    public String getPhrase(){
+        //TODO: Method Stub
+    }
+    public void setPhrase(String phrase){
+        //TODO: Method Stub
+    }
+    
+    public Language getLanguage(){
+        //TODO: Method Stub
+    }
+    
+    public void setLanguage(Language language) {
+        //TODO: Method Stub
+    }
+    public String getTranslation(Language language) {
+        //TODO: Method Stub
+    }
+    public void addTranslations(String phrase, String unknown) {
+        // What is the unknown for?
+        //TODO: Method Stub
+    }
+    public void removetranslation(Language language){
+        //TODO: Method Stub
+    }
+    public String getDefinition(){
+        //TODO: Method Stub
+    }
+    
+    public void setDefinition(String definition){
+        //TODO: Method Stub
+    }
+    
+    
+    
+    
+    public String toString(){
+        //TODO: Method Stub
+    }
+    public boolean equals(Phrase){
+        //TODO: Method Stub
+    }
+    
+}
---
+++ speek/src/main/java/com/narriation/Settings.java
@@ -0,0 +1,18 @@
+package speek.src.main.java.com.narriation;
+
+public class Settings {
+    private boolean notificationsEnabled;
+    private Settings instance;
+    
+    private Settings(){
+        //TODO: Method Stub
+    }
+    
+    public void enableNotifications(){
+        //TODO: Method Stub
+    }
+    public static Settings getInstance(){
+        //TODO: Method Stub
+    } 
+    
+}
