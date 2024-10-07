3fc3dba8dbd79d68c4b97cc8f27dd3f4cfe38fab
John Cicala <johncic12345@gmail.com>
2024-10-07 01:15:50 -0400

    new

---
+++ speek/src/main/java/com/narriation/DataLoader.java
@@ -0,0 +1,15 @@
+package com.narriation;
+import java.util.ArrayList;
+
+public class DataLoader 
+{
+    public ArrayList<Users> getUsers ()
+    {
+
+    }
+
+    public LanguageApplication getLearning ()
+    {
+        
+    }
+}
