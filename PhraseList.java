46869c1f95276e15915de7cfbe78df37646288fa
BuildTools <unconfigured@null.spigotmc.org>
2024-10-07 00:27:11 -0400

    #9 done

---
+++ PhraseList.java
@@ -0,0 +1,44 @@
+public class PhraseList {
+    private phrases List<Phrase>;
+
+    public PhraseList() {
+
+    }
+
+    public void addPhrase(Phrase phrase) {
+
+    }
+
+    public void removePhrase(Phrase phrase) {
+
+    }
+
+    public Phrase getPhrase(int index) {
+
+    }
+
+    public Phrase findPhrase(String searchPhrase) {
+
+    }
+
+    public int getSize() {
+
+    }
+
+    public void printPhrase() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void savePhrases() {
+
+    }
+
+    public void loadPhrases() {
+
+    }
+
+}
