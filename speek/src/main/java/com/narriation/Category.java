2dc332ce8f257c1f32a965428c4a575156e3d8c4
Timothy Young <tpy@email.sc.edu>
2024-10-06 23:19:06 -0400

    Timothy Stubs

---
+++ speek/src/main/java/com/narriation/Category.java
@@ -0,0 +1,21 @@
+public class Category {
+    public String name; 
+    public Language language;
+    public UUID uuid; //implement with real built-in UUID class 
+
+    public void setName(String name) {
+        
+    }
+
+    public String getName() {
+
+    }
+
+    public void setLanguage(Language language) {
+
+    }
+
+    public Language getLanguage() {
+
+    }
+}
---
+++ speek/src/main/java/com/narriation/User.java
@@ -0,0 +1,50 @@
+public class User {
+    public String username;
+    private String email;
+    private String password;
+    public String bio;
+    private List<User> friendsList;
+    public Image profilePicture;
+    private List<Progress> languageProgress;
+    private UUID uuid;
+
+    public void User(String username, String email, String password) {
+
+    }
+
+    public void createAccount() {
+
+    }
+
+    public boolean validLogin() {
+
+    }
+
+    public void forgotPassword() {
+
+    }
+
+    public String addFriend(User user) {
+
+    }
+
+    public String refer() {
+
+    }
+
+    public HashMap<Category, Integer> checkCategoryProgress() {
+
+    }
+
+    public HashMap<Lesson, Integer> checkLessonProgress() {
+
+    }
+
+    public void setProfilePicture(Image pfp) {
+
+    }
+
+    public void saveUser() {
+        
+    }
+}
