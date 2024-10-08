c6eaf99d7030c1477527574dd7f13fd5d72ac47f
Trevor La <trevor@Trevors-MacBook-Air-4.local>
2024-10-08 12:19:44 -0400

    Directory fix and added DataConstants

---
+++ DataLoader.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class DataLoader extends DataConstants
+{
+    public ArrayList<Users> getUsers ()
+    {
+
+    }
+
+    public LanguageApplication getLearning ()
+    {
+
+    }
+}
