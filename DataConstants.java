1ca131fba02cc55cb48ae9df78fd37a63818c697
BuildTools <unconfigured@null.spigotmc.org>
2024-10-13 21:45:18 -0400

    #19 done

--- DataConstants.java
+++ DataConstants.java
@@ -1,15 +1,13 @@
-public class DataConstants 
+import java.util.*;
+public class DataConstants
 {
     protected static final String USER_FILE_NAME = (System.getProperty("os.name").equals("Linux")
     ? "./demo/src/main/java/data/user.json"
     : ".\\demo\\src\\main\\java\\data\\user.json");
     protected static final String USER = "user";
-    protected static final String USER_ID = "user-id";
-    protected static final String USER_FIRST_NAME = "first-name";
-    protected static final String USER_LAST_NAME = "last-name";
-    protected static final String USER_USERNAME = "user-name";
+    protected static final String USER_USERNAME = "username";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_EMAIL = "email";
-    protected static final String USER_PHONE_NUMBER = "phone-number";
-    protected static final String USER_TYPE = "type";
+    protected static final String USER_BIO = "bio";
+    protected static final UUID USER_UUID = UUID.randomUUID();
 }
