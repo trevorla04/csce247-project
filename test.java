2cc6a750ab0b1d5c038a380f319014272c7ea0b6
BuildTools <unconfigured@null.spigotmc.org>
2024-10-14 01:48:05 -0400

    test class fix

--- test.java
+++ test.java
@@ -8,6 +8,7 @@ public class test {
         User user1 = new User("john_doe", "john@example.com", "password123");
         User user2 = new User("jane_smith", "jane@example.com", "securepassword");
         User user3 = new User("alice_wonder", "alice@example.com", "alice1234");
+        User user4 = new User("trevor", "trevor@example.com", "trevor1234");
 
         UserList userList = UserList.getInstance();
         userList.addUser(user1);
@@ -34,23 +35,13 @@ public class test {
                 "password123"));
 
         // Testing valid add user
-        System.out.println("Testing adding a valid user: " +);
+        System.out.println("Testing adding a valid user: " + loadedUsers.getAllUsers().contains(user1));
 
-    }
+        // Testing invalid login
+        System.out.println("Testing an invalid login for user1: " + user1.validLogin("john@example.com",
+                "password"));
 
-    public static void testValidLogin() {
-
-    }
-
-    public static void testInvalidLogin() {
-
-    }
-
-    public static void testValidUser() {
-
-    }
-
-    public static void testInvalidUser() {
-
+        // Testing invalid add user
+        System.out.println("Testing adding an invalid user: " + loadedUsers.getAllUsers().contains(user4));
     }
 }
