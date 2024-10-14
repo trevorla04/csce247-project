7a8d23886325d3996f50f5e662d04bc63e1277e9
BuildTools <unconfigured@null.spigotmc.org>
2024-10-14 01:05:07 -0400

    #14 fix

--- DataWriter.java
+++ DataWriter.java
@@ -14,7 +14,7 @@ public class DataWriter extends DataConstants
 
         for (User user : users.getAllUsers()) {
             JSONObject userJSON = new JSONObject();
-            userJSON.put(USER_USERNAME, user.getUserName());
+            userJSON.put(USER_USERNAME, user.getUsername());
             userJSON.put(USER_EMAIL, user.getEmail());
             userJSON.put(USER_PASSWORD, user.getPassword());
             userJSON.put(USER_UUID, user.getId().toString());
