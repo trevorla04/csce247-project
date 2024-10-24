8bf0db397e14860d69b46637ffc5444f56ce5bfe
Timothy Young <tpy@email.sc.edu>
2024-10-24 16:30:38 -0400

    LanguageUI javadoc

--- speek/src/main/java/com/backend/LanguageUI.java
+++ speek/src/main/java/com/backend/LanguageUI.java
@@ -3,17 +3,29 @@ package com.backend;
 import java.util.Scanner;
 
 public class LanguageUI {
+    // Scanner for reading user input
     private Scanner scanner;
+    // Reference to the language application
     private LanguageApplication application;
 
+    /**
+     * Starts the main loop of the language learning application.
+     * This method serves as the entry point for running the user interface.
+     */
     public void run() {
         // TODO Driver
     }
 
+    /**
+     * Displays the main menu of the application to the user.
+     * The main menu will present different options for interacting with the language learning system,
+     * such as choosing a language, starting lessons, and viewing progress.
+     */
     private void displayMainMenu() {
         // TODO create main menu
     }
 
+    // Exits the application, performing any necessary cleanup such as saving user data or progress.
     private void exit() {
         // TODO save and exit
     }
