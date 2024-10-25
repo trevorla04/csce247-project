833af71ef27eea9aab6d01fd83f361b4d8be0aa5
Trevor La <trevor@Trevors-MacBook-Air-4.local>
2024-10-25 16:35:00 -0400

    UserList fix

--- speek/src/main/java/com/backend/UserList.java
+++ speek/src/main/java/com/backend/UserList.java
@@ -1,83 +1,57 @@
 package com.backend;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 public class UserList 
 {
-    private static UserList userList;
-    private HashMap<String,User> userLookup = new HashMap<String,User>();
+    private static UserList users;
+    private List<User> userList;
 
     // Private constructor to prevent instantiation outside of the singleton pattern
-    public UserList() {}
+    public UserList() {
+        users = DataLoader.loadUsers();
+    }
 
     // Retrieves the single instance of UserList. Creates a new instance if none exists
     public static UserList getInstance ()
     {
-        if (userList == null) 
+        if (users == null)
         {
-            userList = new UserList();
+            users = new UserList();
         }
-        return userList;
+        return users;
     }
 
     // Retrieves a user by their username
     public User getUser (String s)
     {
-        userLookup.get(s);
+        for (User user : userList) {
+            if (user.getUsername().equalsIgnoreCase(s))
+                return user;
+        }
         return null;
     }
 
     // Adds a user to the user list
     public void addUser(User user) {
-        userLookup.put(user.username,user);
+        if (!userList.contains(user))
+            userList.add(user);
     }
 
-    public ArrayList<User> getAllUsers() {
-        return users;
+    public List<User> getAllUsers() {
+        return userList;
     }
 
     // Checks if a user with the specified username exists in the user list
     public boolean containsUser(String username){
-        return userLookup.containsKey(username);
-    }
-
-    // Checks if a user with the specified username exists in the user list
-    public boolean containsUser(User user) {
-        return userLookup.containsKey(user.username);
+        if(getUser(username).getUsername().equalsIgnoreCase(username))
+            return true;
+        else
+            return false;
     }
 
     public void clear() {
-        userLookup.clear();
+        userList.clear();
     }
-
-    // this should be handled by the DataWriter class
-//    public void saveUsers ()
-//    {
-//        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("users.dat")))
-//        {
-//            oos.writeObject(users);
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
-
-    // And this by the data writer class;
-//    public void loadUsers ()
-//    {
-//        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("users.dat")))
-//        {
-//            users = (ArrayList<User>) ois.readObject();
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
 }
