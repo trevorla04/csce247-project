da34b74b616ce51baaa93df2b0b32adc64b0e55c
John Cicala <johncic12345@gmail.com>
2024-10-27 16:27:49 -0400

    new

--- speek/src/main/java/com/backend/UserList.java
+++ speek/src/main/java/com/backend/UserList.java
@@ -3,14 +3,14 @@ package com.backend;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserList 
+public class UserList
 {
     private static UserList users;
     private List<User> userList;
 
     // Private constructor to prevent instantiation outside of the singleton pattern
-    private UserList() {
-        //users = DataLoader.loadUsers();
+    public UserList() {
+        users = DataLoader.loadUsers();
     }
 
     // Retrieves the single instance of UserList. Creates a new instance if none exists
@@ -41,6 +41,31 @@ public class UserList
 
     public List<User> getAllUsers() {
         return userList;
+    }
+
+    public boolean validLogin(String loginUsername, String loginPassword) {
+        if (!users.containsUser(loginUsername)) {
+            if (users.getUser(loginUsername).getPassword().equals(loginPassword))
+                return true;
+            else
+                return false;
+        } else
+            return false;
+
+    }
+
+    /**
+     * Creates a new account for the user.
+     */
+    public boolean createAccount(String username, String email, String password) {
+        if(users.containsUser(username)){
+            System.out.println("User with this username already exists.");
+            return false;
+        }
+        User newUser = new User(username, email, password);
+        users.addUser(newUser);
+        System.out.println("New user created with username: " + newUser.getUsername());
+        return true;
     }
 
     // Checks if a user with the specified username exists in the user list
